aa92d7b07b34c30bfef3305030624e0083028e64
sc372 <372lsc@gmail.com>
2019-05-27 01:16:51 +0900

    role init

--- src/main/java/sc372/springbootjwtjava/repository/RoleRepository.java
+++ src/main/java/sc372/springbootjwtjava/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package sc372.springbootjwtjava.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sc372.springbootjwtjava.domain.Role;
 
 public interface RoleRepository extends JpaRepository<Role, String>{
-    Role findByName(String name);
+    List<Role> findByName(String name);
 }
--- src/main/java/sc372/springbootjwtjava/service/impl/UserServiceImpl.java
+++ src/main/java/sc372/springbootjwtjava/service/impl/UserServiceImpl.java
@@ -7,11 +7,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import sc372.springbootjwtjava.domain.User;
-// import sc372.springbootjwtjava.domain.UserRoles;
 import sc372.springbootjwtjava.dto.UserDto;
 import sc372.springbootjwtjava.repository.RoleRepository;
 import sc372.springbootjwtjava.repository.UserRepository;
-// import sc372.springbootjwtjava.repository.UserRolesRepository;
 import sc372.springbootjwtjava.service.UserService;
 
 @Service(value = "userService")
@@ -23,20 +21,12 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private RoleRepository roleRepository;
 
-    // @Autowired
-    // private UserRolesRepository userRolesRepository;
-
     @Override
     public User createUser(UserDto userDto) {
         User newUser = new User();
         newUser.setUserEmail(userDto.getUserEmail());
         newUser.setPassword(new BCryptPasswordEncoder().encode(userDto.getPassword()));
-        newUser.setRoles(new HashSet<>(roleRepository.findAll()));
-
-        // UserRoles userRoles = new UserRoles();
-        // userRoles.setUsersId(newUser.getId().toString());
-        // userRoles.setRolesId(roleRepository.findByName(userDto.getRoleName()).getId().toString());
-        // userRolesRepository.save(userRoles);
+        newUser.setRoles(new HashSet<>(roleRepository.findByName(userDto.getRoleName())));
 
         return userRepository.save(newUser);
     }
